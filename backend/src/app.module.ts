9c96df9f66c5fb50f76167579c88975a8e2b8494
anchal.paswan <anchal.paswan@genesistechnologies.in>
2024-08-08 15:47:47 +0530

    created user entity

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { DataSource } from 'typeorm';
       autoLoadEntities: true,
       synchronize: true,
     }),
+    UserModule,
   ],
 })
 
