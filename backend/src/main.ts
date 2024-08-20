937dc19658e8ccc07106f4d774264e8bd1c31c2c
anchal.paswan <anchal.paswan@genesistechnologies.in>
2024-08-20 12:04:10 +0530

    swagger setup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './Filter/all-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Tourism Website ')
+    .setDescription('API for accessing the tourism website')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
 
