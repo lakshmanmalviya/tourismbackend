d5b74cb2e052d893e0b023c0e68a02f4deeee90f
anchal.paswan <anchal.paswan@genesistechnologies.in>
2024-08-23 16:25:35 +0530

    created Delete service and controller for place

--- backend/src/place/place.controller.ts
+++ backend/src/place/place.controller.ts
@@ -8,14 +8,16 @@ import {
     BadRequestException,
     Patch,
     Param,
+    Delete,
+    NotFoundException,
   } from '@nestjs/common';
   import { FilesInterceptor } from '@nestjs/platform-express';
   import { PlaceService } from './place.service';
   import { CreatePlaceDto } from './dto/create-place.dto';
-  import { UpdatePlaceDto } from './dto/update-place.dto'; 
+  import { UpdatePlaceDto } from './dto/update-place.dto';
   import { AuthGuard } from '../auth/auth.guard';
-  import { RolesGuard } from '../role/role.guard'; 
-  import { Roles } from '../role/roles.decorator'; 
+  import { RolesGuard } from '../role/role.guard';
+  import { Roles } from '../role/roles.decorator';
   
   @Controller('places')
   export class PlaceController {
@@ -53,7 +55,10 @@ import {
       @Body() updatePlaceDto: UpdatePlaceDto,
     ) {
       try {
-        const updatedPlace = await this.placeService.updatePlace(id, updatePlaceDto);
+        const updatedPlace = await this.placeService.updatePlace(
+          id,
+          updatePlaceDto,
+        );
         return {
           statusCode: 200,
           message: 'Place updated successfully',
@@ -61,6 +66,23 @@ import {
         };
       } catch (error) {
         throw new BadRequestException('Error updating place');
+      }
+    }
+  
+    @UseGuards(AuthGuard, RolesGuard)
+    @Delete(':id')
+    async deletePlace(@Param('id') id: number) {
+      try {
+        await this.placeService.deletePlace(id);
+        return {
+          statusCode: 200,
+          message: 'Place and associated images deleted successfully',
+        };
+      } catch (error) {
+        if (error instanceof NotFoundException) {
+          throw new NotFoundException(error.message);
+        }
+        throw new BadRequestException('Error deleting place');
       }
     }
   }
