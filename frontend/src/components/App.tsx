4312079a915afc21298cd38c16019c3b32b96369
anchal.paswan <anchal.paswan@genesistechnologies.in>
2024-08-26 16:37:13 +0530

    created hero section with responsive design and typewriter effect

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import HeroSection from "./HeroSection";
 const App = () => {
-  return <div className="flex flex-col min-h-screen bg-green-200">this is body</div>;
+  return <div className="flex flex-col min-h-screen bg-green-200">
+
+    <HeroSection/>
+  </div>;
 };
 
 export default App;
