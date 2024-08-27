5b41f4ab1533579af9844f86a791fecd587e8c34
anchal.paswan <anchal.paswan@genesistechnologies.in>
2024-08-27 15:42:43 +0530

    designed contact form ui

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import HeroSection from "./HeroSection";
 import PortalDescription from "./PortalDescription";
 import TopPlaceSlider from "./TopPlaceSlider";
+import ContactForm from "./ContactForm";
+
 const App = () => {
-  return <div className="flex flex-col min-h-screen">
-    <HeroSection/>
-    <PortalDescription/>
-    <TopPlaceSlider/>
-  </div>;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <HeroSection />
+      <PortalDescription />
+      <TopPlaceSlider />
+      <ContactForm />
+    </div>
+  );
 };
 
 export default App;
