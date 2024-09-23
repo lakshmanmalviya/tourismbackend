7014549410182ec01237a708b46405f828557aa2
anchal.paswan <anchal.paswan@genesistechnologies.in>
2024-09-23 18:23:44 +0530

    integrated heritage api with frontend

--- frontend/src/pages/heritages/[id].tsx
+++ frontend/src/pages/heritages/[id].tsx
@@ -1,45 +1,69 @@
-import HeritageThumbnailAnimation from '@/components/Heritage/HeritageThumbnailAnimation'
-import DescriptionMapSlider from "@/components/common/DescriptionMapslider"
-import React from 'react'
-import Slider from 'react-slick';
+import { useState, useEffect } from "react";
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Image from 'next/image';
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useAppDispatch, useAppSelector } from "@/hooks/hook";
+import { RootState } from "../../Redux/store";
+import { fetchHeritageByIdRequest } from "@/Redux/slices/heritageSlice";
+import { Heritage } from "@/types/heritage/heritagePayload";
+import HeritageThumbnailAnimation from "@/components/Heritage/HeritageThumbnailAnimation";
+import DescriptionMapSlider from "@/components/common/DescriptionMapslider";
 
 const HeritageDetails = () => {
-    const settings = {
-        dots: false,
-        infinite: true,
-        speed: 700,
-        autoplay: true,
-        fade: true,
-        autoplaySpeed: 3000,
-        pauseOnHover: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      };
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+  const data = useAppSelector((state: RootState) => state.heritage.heritage);
+  const [heritage, setHeritage] = useState<Heritage | null>(null);
 
-      
-const images = [
-    'https://images.unsplash.com/photo-1682687220566-5599dbbebf11?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80',
-    'https://images.unsplash.com/photo-1682687221248-3116ba6ab483?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80',
-    'https://images.unsplash.com/photo-1688362809005-e1d27bf427ac?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80',
-    'https://images.unsplash.com/photo-1688362892497-b2e0746eb5e5?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80',
-    'https://images.unsplash.com/photo-1688362892512-01f4f9618199?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80',
-  ]
+  useEffect(() => {
+    const { id } = router.query;
+    if (id) {
+      dispatch(fetchHeritageByIdRequest(id as string));
+    }
+  }, [dispatch, router.query]);
+
+  useEffect(() => {
+    if (data) {
+      setHeritage(data);
+    }
+  }, [data]);
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 700,
+    autoplay: true,
+    fade: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <>
-    <HeritageThumbnailAnimation name="Heritage" thumbnail=""/>
-    <DescriptionMapSlider title={"heritage"}description="thi si sdescription" mapUrl={""}/>
-    <div className='relative'>
-      <Slider {...settings} className="group">
-          {
-           images?.map((img, index) => (
+      <HeritageThumbnailAnimation
+        name={heritage?.name}
+        thumbnail={heritage?.thumbnailUrl}
+      />
+
+      <DescriptionMapSlider
+        title={heritage?.name ?? ""}
+        description={heritage?.description ?? ""}
+        mapUrl={heritage?.mapUrl ?? ""}
+      />
+
+      {heritage && (
+        <div className="relative">
+          <Slider {...settings} className="group">
+            {heritage.images?.map((img, index) => (
               <div key={index} className="relative">
                 <div
                   className="absolute inset-0 -z-10 overflow-hidden"
                   style={{
-                    backgroundImage: `url(${img})`,
+                    backgroundImage: `url(${img.link})`,
                     backgroundSize: "cover",
                     backgroundPosition: "center",
                     filter: "blur(10px)",
@@ -47,7 +71,7 @@ const images = [
                   }}
                 />
                 <Image
-                  src={img}
+                  src={img.link}
                   alt="place images"
                   width={700}
                   height={600}
@@ -55,11 +79,11 @@ const images = [
                 />
               </div>
             ))}
-        </Slider>
-
-      </div>
+          </Slider>
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default HeritageDetails
+export default HeritageDetails;
